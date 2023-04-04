2a99bc7d739ac6d598df0442fbda2991ce21229a
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2023-04-04 13:11:50 -0600

    add routes

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,15 @@ import NavBar from "../NavBar/NavBar";
 import Home from "../Home/Home";
 import "./App.css";
 import Footer from "../Footer/Footer";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <section className="App">
       <NavBar />
-      <Home />
+      <Route exact path="/">
+        <Home />
+      </Route>
       <Footer />
     </section>
   );
