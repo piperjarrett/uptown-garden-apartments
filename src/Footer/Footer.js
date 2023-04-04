69c449b3f7a8f18478b7c0b66d59de4931f69e36
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2023-04-04 11:46:49 -0600

    add footer styling

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,16 +4,18 @@ const Footer = () => {
   return (
     <footer>
       <img src={horizontalLogo} alt="apartment-logo" />
-      <div className="footer-contact">
-        <p>Contact Us</p>
-        <p>uptowngardensabq@gmail.com</p>
-        <p>(505)-948-8299</p>
-      </div>
-      <div>
-        <p>Leasing Office</p>
-        <p>935 N Penn</p>
-        <p>Mon - Friday: 9am - 6pm</p>
-        <p>Sat & Sun: 11:00am - 5pm</p>
+      <div className="footer-info">
+        <div className="footer-contact">
+          <h4>Contact Us</h4>
+          <p>uptowngardensabq@gmail.com</p>
+          <p>(505)-948-8299</p>
+        </div>
+        <div>
+          <h4>Leasing Office</h4>
+          <p>935 N Penn</p>
+          <p>Mon - Friday: 9am - 6pm</p>
+          <p>Sat & Sun: 11:00am - 5pm</p>
+        </div>
       </div>
     </footer>
   );
