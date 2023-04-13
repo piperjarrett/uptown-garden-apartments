bead64401a89043996a63e1e5cc0b02375a774c2
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2023-04-13 12:09:57 -0600

    add schools nearby

--- src/AboutLocation/AboutLocation.js
+++ src/AboutLocation/AboutLocation.js
@@ -3,28 +3,43 @@ import "./AboutLocation.css";
 const AboutLocation = () => {
   return (
     <div className="about-location">
-      <h2>Nearby Uptown Garden Apartments</h2>
-      <div className="locattion-education">
-        <h3>Education</h3>
-        <p>University of New Mexico</p>
-        <p>Drive: 10 min</p>
-        <p>4 mi.</p>
-        <p>Central New Mexico University</p>
-        <p>Drive: 10 min</p>
-        <p>4 mi.</p>
+      <div className="about-location-top">
+        <h2>Nearby Uptown Garden Apartments</h2>
+        <div className="location-education">
+          <h3>Education</h3>
+          <div className="college">
+            <p>University of New Mexico</p>
+            <p>Drive: 10 min</p>
+            <p>4 mi.</p>
+          </div>
+          <div className="college">
+            <p>Central New Mexico University</p>
+            <p>Drive: 10 min</p>
+            <p>4 mi.</p>
+          </div>
+        </div>
       </div>
       <div className="location-public-schools">
         <h3>Public Schools</h3>
-        <p>Mark Twain</p>
-        <p>Public Elementry School</p>
-        <p>Grades: PK-5th</p>
-        <p>MHayes Middle</p>
-        <p>Public Middle School</p>
-        <p>Grades: 6th-8th</p>
-        <p>Highlands High</p>
-        <p>Public High School</p>
-        <p>Grades: 9th-12thh</p>
+        <div className="public-schools">
+          <div className="school">
+            <p>Mark Twain</p>
+            <p>Public Elementry School</p>
+            <p>Grades: PK-5th</p>
+          </div>
+          <div className="school">
+            <p>Hayes Middle</p>
+            <p>Public Middle School</p>
+            <p>Grades: 6th-8th</p>
+          </div>
+          <div className="school">
+            <p>Highlands High</p>
+            <p>Public High School</p>
+            <p>Grades: 9th-12thh</p>
+          </div>
+        </div>
       </div>
+      <h3>On a Map</h3>
     </div>
   );
 };
