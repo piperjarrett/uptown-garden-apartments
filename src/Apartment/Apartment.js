05a7a2fcca184370bdb17d9895086b29d1573de7
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2023-04-28 11:39:10 -0600

    add modal functionality

--- src/Apartment/Apartment.js
+++ src/Apartment/Apartment.js
@@ -1,20 +1,32 @@
 import "./Apartment.css";
 import layout from "../assets/layout.png";
+import images from "../assets/images.svg";
 import { useEffect, useState } from "react";
 import { apartmentRequest } from "../apiCalls/apiCalls";
+import ReactModal from "react-modal";
+import ModalContent from "../Modal/ModalContent";
 
 const Apartment = ({ apartmentId }) => {
   const [apartment, setApartment] = useState([]);
   const [error, setError] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   useEffect(() => {
     apartmentRequest(apartmentId)
       .then((data) => setApartment(data))
       .catch((err) => setError("There was an error"));
+
     window.scrollTo(0, 0);
   }, []);
 
   const openEmail = () => {
     window.open("mailto:uptowngardensabq@gmail.com?");
+  };
+  const handleImageClick = () => {
+    setIsModalOpen(true);
+  };
+  const handleModalClose = () => {
+    setIsModalOpen(false);
   };
 
   return apartment.length === 0 ? (
@@ -43,10 +55,24 @@ const Apartment = ({ apartmentId }) => {
             ) : (
               <h4>{apartment.floors} floors</h4>
             )}
+            <img
+              src={images}
+              onClick={handleImageClick}
+              className="modal-image"
+            />
+            <ReactModal isOpen={isModalOpen} onRequestClose={handleModalClose}>
+              <ModalContent
+                apartmentImages={Object.keys(apartment.photos).map((key) => {
+                  return apartment.photos[key];
+                })}
+              />
+            </ReactModal>
           </div>
         </div>
         <div className="rent-price">
-          <h4 className="header-rent-price">${apartment.rent}/month</h4>
+          <div className="price-and-modal">
+            <h4 className="header-rent-price">${apartment.rent}/month</h4>
+          </div>
           <div className="ind-apartment-buttons">
             <a href={process.env.PUBLIC_URL + "/New Application.pdf"} download>
               <button onClick={openEmail}>Download Application</button>
