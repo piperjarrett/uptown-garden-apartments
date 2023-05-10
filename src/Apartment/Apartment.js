14493e8c48526e47cf47f2bc92892115ed223200
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2023-05-10 17:54:48 -0600

    change apartment page

--- src/Apartment/Apartment.js
+++ src/Apartment/Apartment.js
@@ -5,7 +5,22 @@ import { useEffect, useState } from "react";
 import { apartmentRequest } from "../apiCalls/apiCalls";
 import ReactModal from "react-modal";
 import imagesIcon from "../assets/imagesIcon.png";
-import ModalContent from "../Modal/ModalContent";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/swiper-bundle.min.css";
+import { SwiperSlide } from "swiper/react";
+import { Swiper } from "swiper/react";
+
+import {
+  Navigation,
+  Keyboard,
+  Scrollbar,
+  Mousewheel,
+  Pagination,
+} from "swiper";
 import closeButton from "../assets/form-close.svg";
 
 const Apartment = ({ apartmentId }) => {
@@ -21,6 +36,9 @@ const Apartment = ({ apartmentId }) => {
     window.scrollTo(0, 0);
   }, []);
 
+  // const apartmentImages = Object.keys(apartment?.photos).map((key) => {
+  //   return apartment.photos[key];
+  // });
   const openEmail = () => {
     window.open("mailto:uptowngardensabq@gmail.com?");
   };
@@ -38,7 +56,53 @@ const Apartment = ({ apartmentId }) => {
   ) : (
     <div className="ind-apartment">
       <div className="ind-apartment-top">
-        <div
+        <div className="swiper-container">
+          <div className="modal-images">
+            <Swiper
+              className="swiper-slide"
+              style={{
+                "--swiper-navigation-size": "15px",
+              }}
+              modules={[
+                Navigation,
+                Mousewheel,
+                Keyboard,
+                Scrollbar,
+                Pagination,
+              ]}
+              slidesPerView={1}
+              navigation={true}
+              keyboard={true}
+              mousewheel={true}
+              pagination={{
+                el: ".swiper-pagination",
+                clickable: true,
+              }}
+              // navigation={{
+              //   nextEl: ".swiper-button-next",
+              //   prevEl: ".swiper-button-prev",
+              // }}
+            >
+              {Object.keys(apartment?.photos)
+                .map((key) => {
+                  return apartment.photos[key];
+                })
+                .map((image, index) => (
+                  <SwiperSlide key={index}>
+                    <div className="modal-swiper-div">
+                      <img
+                        className="apartment-image-swiper"
+                        src={image}
+                        alt={`Image ${index}`}
+                      />
+                    </div>
+                  </SwiperSlide>
+                ))}
+              <div className="swiper-pagination"></div>
+            </Swiper>
+          </div>
+        </div>
+        {/* <div
           className="background-image"
           style={{
             backgroundImage: `url(${apartment.community_preview})`,
@@ -46,19 +110,30 @@ const Apartment = ({ apartmentId }) => {
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
           }}
-        >
-          <div className="ind-apartment-info">
-            <h2>
-              {apartment.bedrooms} Bed, {apartment.bathrooms} Bath
-            </h2>
+        > */}
+        <div className="ind-apartment-info">
+          <h2>
+            {apartment.bedrooms} Bedrooms | {apartment.bathrooms} Bathrooms
+          </h2>
+          <div className="apartment-details">
             <h4>{apartment.sqft} Sq. Ft.</h4>
             {apartment.floors === 1 ? (
               <h4>{apartment.floors} floor</h4>
             ) : (
               <h4>{apartment.floors} floors</h4>
             )}
+            <h4>
+              ${apartment.rent}/month {apartmentId <= 4 ? " + utilities" : ""}
+            </h4>
           </div>
-          <div>
+          <div className="ind-apartment-buttons">
+            <a href={process.env.PUBLIC_URL + "/New Application.pdf"} download>
+              <button onClick={openEmail}>Download Application</button>
+            </a>
+            <button>Contact and Book a Tour</button>
+          </div>
+        </div>
+        {/* <div>
             <img
               src={imagesIcon}
               onClick={handleImageClick}
@@ -77,21 +152,21 @@ const Apartment = ({ apartmentId }) => {
                 })}
               />
             </ReactModal>
-          </div>
-        </div>
-        <div className="rent-price">
-          <div className="price-and-modal">
-            <h4 className="header-rent-price">
+          </div> */}
+        {/* </div> */}
+        {/* <div className="rent-price"> */}
+        {/* <div className="price-and-modal"> */}
+        {/* <h4 className="header-rent-price">
               ${apartment.rent}/month {apartmentId <= 4 ? " + utilities" : ""}
             </h4>
-          </div>
-          <div className="ind-apartment-buttons">
+          </div> */}
+        {/* <div className="ind-apartment-buttons">
             <a href={process.env.PUBLIC_URL + "/New Application.pdf"} download>
               <button onClick={openEmail}>Download Application</button>
             </a>
             <button>Contact and Book a Tour</button>
-          </div>
-        </div>
+          </div> */}
+        {/* </div> */}
       </div>
       <div className="ind-apartment-bottom">
         <img className="apartment-layout" src={layout} alt="apartment-layout" />
--- src/App/App.js
+++ src/App/App.js
@@ -11,27 +11,29 @@ function App() {
   return (
     <section className="App">
       <NavBar />
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route
-        path="/complex/:id"
-        render={({ match }) => <Complexes complexName={match.params.id} />}
-      ></Route>
-      {/* <Route path="/Dallas">
+      <div className="main">
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route
+          path="/complex/:id"
+          render={({ match }) => <Complexes complexName={match.params.id} />}
+        ></Route>
+        {/* <Route path="/Dallas">
         <Complexes />
       </Route>
       <Route path="/Marble">
         <Complexes />
       </Route> */}
-      <Route
-        path="/:id/:id/:id"
-        render={({ match }) => <Apartment apartmentId={match.params.id} />}
-      ></Route>
-      <Route path="/location">
-        <AboutLocation />
-      </Route>
-      <Footer />
+        <Route
+          path="/:id/:id/:id"
+          render={({ match }) => <Apartment apartmentId={match.params.id} />}
+        ></Route>
+        <Route path="/location">
+          <AboutLocation />
+        </Route>
+        <Footer />
+      </div>
     </section>
   );
 }
